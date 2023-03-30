9884fb7cffa5c82c24483636c6e52c0bdec0dc1a
Qingyang Zhou <115679515+zhouoh@users.noreply.github.com>
2023-03-30 10:06:50 -0700

    Add files via upload

--- TUI.cpp
+++ TUI.cpp
@@ -9,6 +9,7 @@
 #include "render.h"
 #include "readGout.h"
 #include <menu.h>
+#include <panel.h>
 // Read the xyz file and store it in a vector
 std::vector<std::string> readXYZFile(std::string filename)
 {
@@ -93,6 +94,70 @@ std::vector<std::vector<double>> getCoordinates(std::vector<std::string> line)
     return coordinates;
 }
 
+// Draw the optimization criteria
+WINDOW *drawOptimizationCriteria(int x, int y, std::string type, std::vector<double> value)
+{
+    WINDOW* win = newwin(12, 40, x, y);
+    box(win, 0, 0);
+    
+    mvwprintw(win, 1, 18, type.c_str());
+
+    int n_frames = value.size();
+    std::cout << n_frames << std::endl;
+    double scale;
+    double max = 0;
+    for (int i = 0; i < n_frames; i++)
+    {
+        std::cout << value[i] << std::endl;
+        if (value[i] > max)
+        {
+            max = value[i];
+        }
+    }
+    double min = max;
+    for (int i = 0; i < n_frames; i++)
+    {
+        if (value[i] < min)
+        {
+            min = value[i];
+        }
+    }
+    scale = 9.0 / (max - min);
+    std::vector<std::vector<char>> canvas;
+    for (int i = 0; i < 12; i++)
+    {
+        std::vector<char> temp;
+        for (int j = 0; j < n_frames; j++)
+        {
+            temp.push_back(' ');
+        }
+        canvas.push_back(temp);
+    }
+    for (int i = 0; i < n_frames; i++)
+    {
+        int x = i;
+        int y = 10 - value[i] * scale;
+        std::cout << x << std::endl;
+        canvas[y][x] = '*';
+    }
+    for (std::vector<char> i : canvas)
+    {
+        for (char j : i)
+        {
+            std::cout << j;
+        }
+        std::cout << std::endl;
+    }
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < n_frames; j++)
+        {
+            mvwprintw(win, i + 2, j + 1, "%c", canvas[i][j]);
+        }
+    }
+    wrefresh(win);
+    return win;
+}
 
 
 int main()
@@ -224,7 +289,7 @@ int main()
     }
     #endif
 
-    #ifdef GOUT
+    //#ifdef GOUT
     std::string filename = "TS4.out";
     std::vector<std::vector<std::vector<double>>> frames = readGoutFile(filename);
     int nFrames = frames.size();
@@ -247,15 +312,19 @@ int main()
     int x, y;
     x = LINES;
     y = COLS;
+
     renderResult = render(elementName, coordinates, bondConnectivity, x, y, scale_ratio, x_offset, y_offset);
     printCanvas(renderResult);
     mvprintw(0, 0, "Frame: %d/%d", currentFrame, nFrames);
     mvprintw(x-1, 0, "Press 'q' to quit, 'r' to reset, 'w' to rotate up, 's' to rotate down, 'a' to rotate left, 'd' to rotate right, 'z' to zoom in, 'x' to zoom out, 'i' to move up, 'k' to move down, 'j' to move left, 'l' to move right, 'n' to go to next frame, 'p' to go to previous frame");
+    refresh();
+    
+
     if (ifOPT)
     {
         mvprintw(x-2, 0, "Optimization criteria: MF %.2f RF %.2f MD %.2f RD %.2f", optCriteria[currentFrame][0], optCriteria[currentFrame][1], optCriteria[currentFrame][2], optCriteria[currentFrame][3]);
     }
-
+    WINDOW* win = drawOptimizationCriteria(1, y-40,"RF", optCriteria[currentFrame]);
     refresh();
     while (int key = getch())
     {
@@ -429,7 +498,7 @@ int main()
         }
         else if (key == KEY_RIGHT)
         {
-            if (currentFrame == nFrames - 1)
+            if (currentFrame == nFrames - 2)
             {
                 currentFrame = 0;
             }
@@ -469,7 +538,7 @@ int main()
        
 
     }    
-    #endif
+    //#endif
 
     return 0;
 }
